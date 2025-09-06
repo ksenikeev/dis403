08e1e09778204e69aa31b9ad41c859cfb649d9c1
ksenikeev <ksenikeev@mail.ru>
2025-09-06 15:39:49 +0300

    httpserver

---
+++ lab1_01_httpserver/src/main/java/ru/itis/dis403/lab1_01/httpserver/HttpServer.java
@@ -0,0 +1,56 @@
+package ru.itis.dis403.lab1_01.httpserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ */
+public class HttpServer {
+    public static void main(String[] args) {
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(8080);
+            while (true) {
+                // Ожидаем подключения клиента
+                Socket clientSocket = serverSocket.accept();
+                // Дождались клиента
+                // Поток для чтения данных от клиента
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+                // Читаем пакет от клиента
+                String lineOne = reader.readLine();
+                System.out.println(lineOne);
+                String[] components = lineOne.split(" ");
+                String resource = components[1];
+                if (resource.equals("/shutdown")) {
+                    break;
+                }
+                while (true) {
+                    // Читаем пакет от клиента
+                    String message = reader.readLine();
+                    System.out.println(message);
+
+                    if (message.isEmpty()) {
+                        OutputStream os = clientSocket.getOutputStream();
+                        os.write("HTTP/1.1 200 OK\r\n".getBytes());
+                        os.write("Content-Type: text/html;charset=UTF-8\r\n".getBytes());
+                        os.write("\r\n".getBytes());
+                        os.write("<html><body>Hello!</body></html>".getBytes());
+                        os.flush();
+                        break;
+                    }
+                }
+                clientSocket.close();
+
+            }
+            serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
