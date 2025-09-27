dcfa446d813eac331a2bf1246c1ec77f8cba8ea8
ksenikeev <ksenikeev@mail.ru>
2025-09-27 14:48:24 +0300

    lab02 template

--- lab1_02_servlet/src/main/java/ru/itis/dis403/lab1_02/TemplateEngine.java
+++ lab1_02_servlet/src/main/java/ru/itis/dis403/lab1_02/TemplateEngine.java
@@ -7,13 +7,26 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @WebServlet("*.html")
 public class TemplateEngine extends HttpServlet {
 
     final static Logger logger = LogManager.getLogger(TemplateEngine.class);
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         logger.debug(request.getServletPath());
+
+        String fileName = request.getServletPath().substring(1);
+        URL path = TemplateEngine.class.getClassLoader()
+                .getResource("templates/" + fileName);
+
+        String template = Files.readString(Paths.get(path.getPath()));
+
+        response.getWriter().write(template);
     }
 }
---
+++ lab1_02_servlet/src/main/java/ru/itis/dis403/lab1_02/TemplateTest.java
@@ -0,0 +1,27 @@
+package ru.itis.dis403.lab1_02;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+@WebServlet("/template/test")
+public class TemplateTest extends HttpServlet {
+
+    final static Logger logger = LogManager.getLogger(TemplateTest.class);
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.debug(request.getServletPath());
+
+        // Отрабатываем логику приложения
+        // Отрисовка страницы - передаем дальше request
+        request.getRequestDispatcher("/test.html")
+                .forward(request, response);
+    }
+}
