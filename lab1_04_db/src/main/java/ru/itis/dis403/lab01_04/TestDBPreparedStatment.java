7eb94df481c65b8d8a8e7366bdc62403c1ff28e9
ksenikeev <ksenikeev@mail.ru>
2025-10-11 14:56:09 +0300

    lab04 db

---
+++ lab1_04_db/src/main/java/ru/itis/dis403/lab01_04/TestDBPreparedStatment.java
@@ -0,0 +1,40 @@
+package ru.itis.dis403.lab01_04;
+
+import java.sql.*;
+
+public class TestDBPreparedStatment {
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+
+            Connection connection =
+                    DriverManager.getConnection(
+                            // адрес БД , имя пользователя, пароль
+                            "jdbc:postgresql://localhost:5432/demo","postgres","passwd");
+
+            String sql = "select * from bookings.airplanes_data where airplane_code = ? ";
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setString(1, "35X");
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                System.out.print(resultSet.getString("airplane_code") + ";");
+                System.out.print(resultSet.getString("model") + ";");
+                System.out.print(resultSet.getString("range") + ";");
+                System.out.println(resultSet.getString("speed"));
+            }
+
+            resultSet.close();
+
+            statement.close();
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
