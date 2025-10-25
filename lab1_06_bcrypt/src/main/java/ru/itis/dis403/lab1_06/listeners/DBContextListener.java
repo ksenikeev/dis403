64003f4e746c0cab0344b0c70ba6a237d7e5f15e
ksenikeev <ksenikeev@mail.ru>
2025-10-25 17:00:48 +0300

    lab06

--- lab1_06_bcrypt/src/main/java/ru/itis/dis403/lab1_06/listeners/DBContextListener.java
+++ lab1_06_bcrypt/src/main/java/ru/itis/dis403/lab1_06/listeners/DBContextListener.java
@@ -3,19 +3,27 @@ package ru.itis.dis403.lab1_06.listeners;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ru.itis.dis403.lab1_06.repository.DbConnection;
 
 @WebListener
 public class DBContextListener implements ServletContextListener {
+
+    final static Logger logger = LogManager.getLogger(DBContextListener.class);
+
     public void contextInitialized(ServletContextEvent sce) {
+        logger.debug("contextInitialized");
         try {
             DbConnection.init();
         } catch (ClassNotFoundException e) {
+            logger.atError().withThrowable(e).log();
             e.printStackTrace();
         }
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
+        logger.debug("contextDestroyed");
         DbConnection.destroy();
     }
 }
