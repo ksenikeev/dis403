2a360290c9df7a709c96df73687a03d3e00d1849
ksenikeev <ksenikeev@mail.ru>
2025-11-08 14:50:59 +0300

    lab07

--- lab1_07_ajax/src/main/java/ru/itis/dis403/lab1_07/controllers/IndexServlet.java
+++ lab1_07_ajax/src/main/java/ru/itis/dis403/lab1_07/controllers/IndexServlet.java
@@ -1,5 +1,6 @@
 package ru.itis.dis403.lab1_07.controllers;
 
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,19 +8,35 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import ru.itis.dis403.lab1_07.model.City;
+import ru.itis.dis403.lab1_07.repository.CityRepository;
 
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet("/index")
+@WebServlet(urlPatterns = {"/","/index"})
 public class IndexServlet extends HttpServlet {
 
     final static Logger logger = LogManager.getLogger(IndexServlet.class);
 
+    private CityRepository cityRepository;
+
+    public void init() {
+        ServletContext context = getServletContext();
+        cityRepository =
+                (CityRepository) context.getAttribute("cityRepository");
+        logger.debug("cityRepository = " + cityRepository);
+    }
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Сюда может попасть только аутентифицированный пользователь
-        // значит в его сессии должен быть атрибут "user" (который мы положили при аутентификации)
-        request.setAttribute("user", request.getSession().getAttribute("user"));
+
+        try {
+            List<City> cities = cityRepository.getAll();
+            request.setAttribute("cities", cities);
+        } catch (Exception e) {
+            logger.atError().withThrowable(e).log();
+        }
 
         request.getRequestDispatcher("/index.ftlh")
                 .forward(request, response);
--- lab1_07_ajax/src/main/java/ru/itis/dis403/lab1_07/listeners/DBContextListener.java
+++ lab1_07_ajax/src/main/java/ru/itis/dis403/lab1_07/listeners/DBContextListener.java
@@ -1,11 +1,14 @@
 package ru.itis.dis403.lab1_07.listeners;
 
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import ru.itis.dis403.lab1_07.repository.CityRepository;
 import ru.itis.dis403.lab1_07.repository.DbConnection;
+import ru.itis.dis403.lab1_07.repository.StreetRepository;
 
 @WebListener
 public class DBContextListener implements ServletContextListener {
@@ -16,6 +19,15 @@ public class DBContextListener implements ServletContextListener {
         logger.debug("contextInitialized");
         try {
             DbConnection.init();
+
+            CityRepository cityRepository = new CityRepository();
+            StreetRepository streetRepository = new StreetRepository();
+
+            ServletContext context = sce.getServletContext();
+
+            context.setAttribute("cityRepository", cityRepository);
+            context.setAttribute("streetRepository", streetRepository);
+
         } catch (ClassNotFoundException e) {
             logger.atError().withThrowable(e).log();
             e.printStackTrace();
