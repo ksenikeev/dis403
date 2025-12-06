46e5ea40c916cded98b27b31639dd476dcf65364
ksenikeev <ksenikeev@mail.ru>
2025-12-06 16:14:25 +0300

    udp

--- lab1_10_udp/src/main/java/ru/itis/dis403/lab1_10/star/UDPStarClient.java
+++ lab1_10_udp/src/main/java/ru/itis/dis403/lab1_10/star/UDPStarClient.java
@@ -1,11 +1,14 @@
 package ru.itis.dis403.lab1_10.star;
 
 // UDPClient.java
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketTimeoutException;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class UDPStarClient {
@@ -15,6 +18,7 @@ public class UDPStarClient {
     private static final int BUFFER_SIZE = 4096;
 
     public static void main(String[] args) {
+/*
         try (DatagramSocket socket = new DatagramSocket()) {
             socket.setSoTimeout(TIMEOUT);
 
@@ -36,6 +40,53 @@ public class UDPStarClient {
                     break;
                 }
 
+                switch (message) {
+                    case "hello": {
+                        System.out.println("введите имя:");
+                        String name = scanner.nextLine();
+                        byte[] data = name.getBytes(StandardCharsets.UTF_8);
+                        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                        bos.write(0);
+                        bos.write(writeInt(data.length));
+                        bos.write(data);
+
+                        DatagramPacket sendPacket = new DatagramPacket(
+                                bos.toByteArray(),
+                                bos.size(),
+                                serverAddress,
+                                SERVER_PORT
+                        );
+                        socket.send(sendPacket);
+                        break;
+                    }
+                    case "list": {
+                        DatagramPacket sendPacket = new DatagramPacket(
+                                new byte[]{1},
+                                1,
+                                serverAddress,
+                                SERVER_PORT
+                        );
+
+                        socket.send(sendPacket);
+
+                        byte[] receiveData = new byte[BUFFER_SIZE];
+                        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+                        try {
+                            socket.receive(receivePacket);
+                            String response = new String(
+                                    receivePacket.getData(),
+                                    0,
+                                    receivePacket.getLength()
+                            );
+                            System.out.println("Ответ от сервера: " + response);
+                        } catch (SocketTimeoutException e) {
+                            System.out.println("Превышено время ожидания ответа от сервера");
+                        }
+                    }
+
+                }
+
                 // Отправляем сообщение серверу
                 byte[] sendData = message.getBytes();
                 DatagramPacket sendPacket = new DatagramPacket(
@@ -49,20 +100,6 @@ public class UDPStarClient {
                 System.out.println("Сообщение отправлено серверу");
 
                 // Получаем ответ от сервера
-                byte[] receiveData = new byte[BUFFER_SIZE];
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-
-                try {
-                    socket.receive(receivePacket);
-                    String response = new String(
-                            receivePacket.getData(),
-                            0,
-                            receivePacket.getLength()
-                    );
-                    System.out.println("Ответ от сервера: " + response);
-                } catch (SocketTimeoutException e) {
-                    System.out.println("Превышено время ожидания ответа от сервера");
-                }
 
                 System.out.println();
             }
@@ -71,5 +108,24 @@ public class UDPStarClient {
         } catch (IOException e) {
             System.err.println("Ошибка клиента: " + e.getMessage());
         }
+*/
+        int i = 10223234;
+        System.out.println(readInt(writeInt(i)));
     }
+
+    private static byte[] writeInt(int i) {
+        byte[] result = {
+                (byte)((i >>> 24) & 0xFF),
+                (byte)((i >>> 16) & 0xFF),
+                (byte)((i >>> 8) & 0xFF),
+                (byte)(i & 0xFF)
+        };
+        return result;
+    }
+
+    private static int readInt(byte[] data) {
+        return  (data[0] << 24) | (data[1] << 16) | (data[2] << 8) | ((int)data[3]);
+
+    }
+
 }
