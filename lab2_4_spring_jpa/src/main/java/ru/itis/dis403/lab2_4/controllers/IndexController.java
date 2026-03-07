c3882e7764a34653939253ea70f646df0ab1df9a
ksenikeev <ksenikeev@mail.ru>
2026-03-07 16:54:56 +0300

    spring jpa

--- lab2_4_spring_jpa/src/main/java/ru/itis/dis403/lab2_4/controllers/IndexController.java
+++ lab2_4_spring_jpa/src/main/java/ru/itis/dis403/lab2_4/controllers/IndexController.java
@@ -1,13 +1,30 @@
 package ru.itis.dis403.lab2_4.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.itis.dis403.lab2_4.model.Phone;
+import ru.itis.dis403.lab2_4.service.PhoneService;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
 
+    private final PhoneService phoneService;
+
+    public IndexController(PhoneService phoneService) {
+        this.phoneService = phoneService;
+    }
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+
+        List<Phone> phones = phoneService.findAll();
+
+        model.addAttribute("phones", phones);
+
         return "index";
     }
 
